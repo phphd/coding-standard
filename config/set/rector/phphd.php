83eaeb400137bea57a4e3727a922ba9c2591d43a
Yevhen Sidelnyk <zsidelnik@gmail.com>
2024-01-18 19:32:30 +0200

    Skip rector rules: RenameVariableToMatchMethodCallReturnTypeRector,RenameVariableToMatchNewTypeRector,CatchExceptionNameMatchingTypeRector

--- config/set/rector/phphd.php
+++ config/set/rector/phphd.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
 use Rector\Config\RectorConfig;
+use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
 use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
+use Rector\Naming\Rector\ClassMethod\RenameVariableToMatchNewTypeRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\While_\WhileNullableToInstanceofRector;
@@ -33,6 +36,9 @@ return static function (RectorConfig $rectorConfig): void {
             FlipTypeControlToUseExclusiveTypeRector::class,
             RenameParamToMatchTypeRector::class,
             RenamePropertyToMatchTypeRector::class,
+            RenameVariableToMatchMethodCallReturnTypeRector::class,
+            RenameVariableToMatchNewTypeRector::class,
+            CatchExceptionNameMatchingTypeRector::class,
         ],
     );
 };
